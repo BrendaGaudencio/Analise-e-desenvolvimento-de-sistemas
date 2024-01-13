b3f54ddd4b5cde85f7f73d25568df9962e398d2c
Brenda <brenda.gaudencio@outlook.com>
2024-01-13 15:59:57 -0300

    add ex02

---
+++ 1º Semestre - Algortimos e Lógica de Programação/Lista-01/ex-02.c
@@ -0,0 +1,14 @@
+/*2. Crie um programa que imprima o produto de 28 e 43.*/
+
+#include <stdio.h>
+
+int main() {
+    int n1 = 28;
+    int n2 = 43;
+    int resultado;
+
+    resultado = n1 * n2;
+
+    printf("Resultado eh: %d\n", resultado);
+    return 0;
+}
