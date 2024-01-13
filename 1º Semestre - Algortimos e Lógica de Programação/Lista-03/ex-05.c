1f97a55114a0a99e18e3a3b002b5682cf5a72866
Brenda <brenda.gaudencio@outlook.com>
2024-01-13 18:37:15 -0300

    add ex05

---
+++ 1º Semestre - Algortimos e Lógica de Programação/Lista-03/ex-05.c
@@ -0,0 +1,13 @@
+//* 5. Imprimir o intervalo de 120 a 300
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 121; i < 300; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
